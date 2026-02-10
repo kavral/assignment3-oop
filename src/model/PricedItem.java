2ac7afc0ecd066f334fd1925593e013031fba097
user <kamalov.rasul07@gmail.com>
2026-02-10 23:01:16 +0500

    Refactor PricedItem interface by removing unnecessary comments for clarity.

--- src/model/PricedItem.java
+++ src/model/PricedItem.java
@@ -1,14 +1,9 @@
 package model;
 
-/**
- * Narrow interface for priced entities (ISP).
- * Extends Validatable so anything with a price can be validated.
- */
 public interface PricedItem extends Validatable {
 
     double getPrice();
 
-    /** Default: formatted price string. */
     default String getFormattedPrice() {
         return String.format("$%.2f", getPrice());
     }
